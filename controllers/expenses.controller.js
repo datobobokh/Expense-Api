d65d2a1064f44af41e4a3ad0e7c2ee6ea2de4bda
datobobokh <d.bobokhia.websmart@gmail.com>
2022-07-12 11:33:35 +0400

    Added add Feature

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -1,1 +1,16 @@
 const {expenses} = require("../models");
+
+// post
+
+exports.add = (req, res) => {
+    if (!req.body["shop"]) {
+      res.status(422);
+      res.send({answer: "Text is required"});
+    }
+    expenses.create(req.body)
+    .then(async(data) => {
+      res.send(await expenses.findAll());
+    }).catch(err => {
+      res.status(422).send({answer: err});
+    })
+  }
