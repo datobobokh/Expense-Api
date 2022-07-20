5c307784ba6cd92dff1503b4b2bebd1d2ce4979a
datobobokh <d.bobokhia.websmart@gmail.com>
2022-07-20 12:05:29 +0400

    code-style-fixes

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -1,101 +1,97 @@
 const expenses = require("../models");
 
-// post
-
 exports.add = (req, res) => {
   const {shop, spend} = req.body;
   const errorsArr = [];
 
-    if (!shop && !spend) {
-      return res.status(422).send("Shop and Spend input is not defined");
-    } 
-
-    if (isNaN(spend) || (spend < 0)){
-      errorsArr.push("Input is not number or number is not positive");
+  if (!shop && !spend) {
+    return res.status(422).send("Shop and Spend input is not defined");
+  } 
+  if (isNaN(spend) || (spend < 0)) {
+    errorsArr.push("Input is not number or number is not positive");
+  } else {
+    if ( isNaN(spend) || (spend < 0)) {
+      errorsArr.push("Input is not number or number is not positive")
     }
-    if (!shop) {
-      errorsArr.push("shop undefined");
-    }
-
-    if (errorsArr.length) {
-      return res.status(422).send({answer: errorsArr});
-    } 
-
-    expenses.create(req.body)
-    .then((data) => {
-      return this.show(req, res);
-    }).catch(err => {
-      res.status(422).send({answer: err});
-    })
   }
+
+  if (errorsArr.length) {
+    return res.status(422).send({ answer: errorsArr });
+  } 
+
+  expenses.create(req.body)
+  .then((data) => {
+    return this.show(req, res);
+  }).catch(err => {
+    res.status(422).send({ answer: err });
+  })
+}
 
 exports.show = (req, res) => {
   expenses.findAll()
   .then((data) => {
     return res.send(data)
   }).catch(err => {
-    return res.status(422).send({answer: err})
+    return res.status(422).send({ answer: err })
   })
 }
 
 exports.remove = (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
+
   if (!id) {
     res.status(422).send({answer: "Invalid id"})
   }
-  expenses.destroy({where: {id}})
+  expenses.destroy({where: { id }})
   .then((removed) =>  {
-    if(removed) {
+    if (removed) {
       return this.show(req, res);
     }
-      return res.status(404).send({answer: "row not found"})
+      return res.status(404).send({ answer: "row not found" })
   }).catch(err => {
-    res.status(422).send({answer: err});
+    res.status(422).send({ answer: err });
   })
 }
 
 exports.edit = (req, res) => {
-  const {id} = req.params;
-  const {shop, spend} = req.body;
+  const { id } = req.params;
+  const { shop, spend } = req.body;
   const errorsArr = [];
   const valuesObj = {};
+
   if (id) {
     if (!shop && !spend) {
       errorsArr.push("At least one input should be defined");
-    }
-    else {
+    } else {
       if (spend) {
-        if ( isNaN(spend) || (spend < 0)){
+        if (isNaN(spend) || (spend < 0)){
           errorsArr.push("Input is not a number or number is not positive");
-        }
-        else {
+        } else {
           valuesObj.spend = spend
         }
       }
       if (shop) {
-        if(!shop.trim()) {
+        if (!shop.trim()) {
           errorsArr.push("Shop name should be defined");
-        }
-        else {
+        } else {
           valuesObj.shop = shop;
         }
       }
     }
     if (errorsArr.length) {
-      return res.status(422).send({answer: errorsArr})
+      return res.status(422).send({ answer: errorsArr })
     }
     expenses.update(valuesObj, {
-    where: {id: id}})
+    where: { id }})
     .then((edited) => {
-      if(edited) {
+      if (edited) {
         return this.show(req, res);
       }
-      return res.status(404).send({answer: "Info has not Added"}) 
+      return res.status(404).send({ answer: "Info has not Added" }) 
     }).catch(err => {
-      res.status(422).send({answer: err});
+      res.status(422).send({ answer: err });
     })
-  }
-  else {
+  } else {
     res.status(422).send("Id is not defined");
   }
 }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const cors = require('cors');
 const express = require('express');
+
 const app = express();
 const port = 8000;
-
 
 app.use(cors());
 app.use(express.json());
